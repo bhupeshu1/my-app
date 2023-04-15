e1da7f4a4c5d31d0f4bed344f19bbfb193a4cfa4
bhupeshu1 <bhupeshu1@gmail.com>
2023-04-15 18:08:45 +0530

    login form

--- src/Components/Elements/auth/LoginPage/login.js
+++ src/Components/Elements/auth/LoginPage/login.js
@@ -4,63 +4,65 @@ import { Images } from '../../../../Images'
 
 
 const LoginPage = () => {
-	function Add(a,b,c){
-		return a+b+c;
+	function Add(a, b, c) {
+		return a + b + c;
 	}
-	const result=Add(2,2,2)
+	const result = Add(2, 2, 2)
 	console.log(result);
 	console.log('*************************');
-	const [coupenCode,setCoupenCode]=useState("");
+	const [coupenCode, setCoupenCode] = useState("");
+	const [password, setPassword] = useState("");
+	const [visible, setVisible] = useState(false);
 	return (
 		<div className='form-container'>
-			<div className='form-action'>
-				<div className='form'>
-					<form action="">
-						<div className='header-section'>
-							<h1>Log in page</h1>
-						</div>
-						<div className='middle-section'>
-							<div><label htmlFor="email">Email</label><br></br>
-								<input type="text" name="email" placeholder='Enter your email' id="email" />
-							</div>
-							<div className='search'> 
-							<input type="text" onChange={(e) => setCoupenCode(e.target.value)}  value={coupenCode}></input>
-							{ coupenCode ? 
-							<img  src={Images.close_icon}
-									 alt='icon'
-									  style={{width:"20px",height:"20px"}}
-									  onClick={() => setCoupenCode("")}/>
-							:null}
-							
+			<div className='form'>
+				<form action="">
 
-							</div>
-							
+					<div className='header-section'>
+						<h1>Log in</h1>
+					</div>
 
-							<div className='password-section'>
-								{/* <div className='icon-div'>
-								{ passwordType==="password"?
-									<img  src={Images.close_icon}
-									 alt='icon'
-									  style={{width:"20px",height:"20px"}}/> :
-								<img src={Images.open_eye}
-								 alt='icon' 
-								 style={{width:"20px",height:"20px"}}/>
-								 
-	}
-		</div> */}
-		<div><label>Password</label></div>
-		                     <div className='password-wrapper'>
-								<input  type="password" id="ispassword" placeholder='password'></input>
-								<img className="image" id="pass-icon"  src={Images.close_icon}
-									 alt='icon'
-									  style={{width:"20px",height:"20px"}}/>
-							 </div>
-							</div>
-							<button type="submit">Login</button>
+					<div className='middle-section'>
+						<div className='search'>
+							<label htmlFor="email">Email</label><br></br>
+							<input type="email"
+								placeholder='enter your email'
+								onChange={(e) => setCoupenCode(e.target.value)}
+								value={coupenCode}>
+							</input>
+							{coupenCode ?
+								<img id="close-icon" src={Images.cross_icon}
+									alt='icon'
+									style={{ width: "20px", height: "20px" }}
+									onClick={() => setCoupenCode("")} />
+								: null}
 						</div>
-					</form>
-				</div>
+					</div>
+					<div className='password-section'>
+						<div>Password</div>
+						<div className='password-wrapper'>
+							<input id="password"
+								onChange={(e) => setPassword(e.target.value)}
+								value={password}
+								placeholder='password'
+								type={visible ? 'text' : 'password'}
+							>
+
+							</input>
+							<div onClick={() => setVisible(!visible)} className='toggle-div'>
+								{visible ? <img src={Images.open_eye}
+									alt='icon'
+									style={{ width: "20px", height: "20px" }} /> : <img src={Images.close_icon}
+										alt='icon'
+										style={{ width: "20px", height: "20px" }} />}</div>
+						</div>
+					</div>
+					<div className='footer-section'>
+						<button className='button-login' type="submit">Login</button>
+					</div>
+				</form>
 			</div>
+
 		</div>
 
 	)
--- src/Images/index.js
+++ src/Images/index.js
@@ -1,5 +1,6 @@
 export const Images ={
     close_icon:"images/close_icon.png",
-    open_eye:"images/open_eye.png"
+    open_eye:"images/open_eye.png",
+    cross_icon:"images/gallery_close_icon.png"
 
 }
